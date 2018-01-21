0dffe51c8115f21b983760175c9548be5c014a24
arjunashok12 <35652090+arjunashok12@users.noreply.github.com>
2018-01-21 10:19:56 +0000

    Create pos.c

---
+++ pos.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+void main()
+{
+ int a;
+ clrscr();
+ prinf("Enter the number:");
+ scanf("%d",&a);
+ if(a>0)
+  printf("positive");
+ else
+  printf("negative");
+ getch();
+}
+ 
