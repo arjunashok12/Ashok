e7b386cd56225a357a8459ea85b72870c105e0f7
arjunashok12 <35652090+arjunashok12@users.noreply.github.com>
2018-01-25 15:47:45 +0530

    Update alphabet.c

--- alphabet.c
+++ alphabet.c
@@ -5,7 +5,7 @@ void main()
     printf("Give an input:");
     scanf("%c",&c);
     if((c>='a'&&c<='z')||(c>='A'&&c<='Z'))
-    printf("%c is an alphabet",c);
+    printf("\n%c is an alphabet",c);
     else
-    printf("%c is not an alphabet",c);
+    printf("\n%c is not an alphabet",c);
 }
