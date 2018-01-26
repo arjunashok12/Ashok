42604e43207ea34ae25d2a3b470cfce5226dc7c3
arjunashok12 <35652090+arjunashok12@users.noreply.github.com>
2018-01-26 06:52:31 +0000

    Update arraysum.c

--- arraysum.c
+++ arraysum.c
@@ -4,14 +4,14 @@ void main()
     int a[1000],i,j,n,k,sum=0;
     printf("Enter the number of terms:");
     scanf("%d",&n);
-    for(i=0;i<n;i++);
+    for(i=0;i<n;i++)
     {
     printf("Enter the number:");
     scanf("%d",&a[i]);
     }
-    printf("Enter the number of terms to get sum");
+    printf("Enter the number of terms to get sum:");
     scanf("%d",&j);
-    for(k=0;k<j;k++);
+    for(k=0;k<j;k++)
     sum=sum+a[k];
     printf("The sum of %d numbers is:%d",j,sum);
 
