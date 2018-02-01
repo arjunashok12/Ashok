14f600bed330edf4e59415a70a74794740709956
arjunashok12 <35652090+arjunashok12@users.noreply.github.com>
2018-02-01 16:46:39 +0530

    Create odd.c

---
+++ odd.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,i;
+    printf("Enter the limit:");
+    scanf("%d%d",&a,&b);
+    for(i=a;a<=b;a++)
+    {
+    if(a%2!=0)
+    printf("\n%d",a);
+    }
+}
