b3f07ec0ebee36f7edfb97335e42c02e525e1de2
arjunashok12 <35652090+arjunashok12@users.noreply.github.com>
2018-03-02 14:40:31 +0530

    Create count without space.c

---
+++ count without space.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+main()
+{
+    int b=0,i,c=0;
+    char a[1000];
+    printf("Enter the string:");
+    scanf("%[^\n]",&a);
+    for(i=0;a[i]!='\0';i++)
+    {
+     c++;
+     if(a[i]==' ')
+     b++;
+     }
+    printf("Number of characters:%d",c-b);
+}
